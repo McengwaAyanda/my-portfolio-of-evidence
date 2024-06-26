073f05dd63526bfbbca5d507e39e28c4655ef0c7
McengwaAyanda <McengwaAyanda>
2024-06-26 14:21:07 +0200

    new layout changes and removing other pages

---
+++ script.js
@@ -0,0 +1,8 @@
+//Navigation hamburger
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navLinks.classList.toggle("active");
+});
